a4fad98fec0962da9d5ad787927460777d4c0fe1
vishalb <vishal.bhandare@loylty.com>
2017-11-22 17:33:12 +0530

    test

---
+++ FirstProject/SwiftBasics/MovieDetails.swift
@@ -0,0 +1,22 @@
+//
+//  MovieDetails.swift
+//  SwiftBasics
+//
+//  Created by adhirajs on 22/11/17.
+//  Copyright © 2017 vishalb. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetails: NSObject {
+    
+    var POs
+
+}
+{
+    Poster = "https://images-na.ssl-images-amazon.com/images/M/MV5BY2Y0N2YyOTQtMGNiYy00ZDViLTgxYmItODkxNzlkMWVjNTFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg";
+    Title = "She's All That";
+    Type = movie;
+    Year = 1999;
+    imdbID = tt0160862;
+}
