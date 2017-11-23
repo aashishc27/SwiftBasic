91bd5c4615470af955b43bd67471211150594300
vishalb <vishal.bhandare@loylty.com>
2017-11-23 18:01:25 +0530

    pod file

---
+++ FirstProject/Pods/Target Support Files/Pods-SwiftBasics/Pods-SwiftBasics-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftBasics : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftBasics
+@end
---
+++ FirstProject/Pods/Target Support Files/Pods-SwiftBasicsTests/Pods-SwiftBasicsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftBasicsTests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftBasicsTests
+@end
